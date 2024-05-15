94a8c7b7e2565f51bf9f95842751e7f99ba4a6bc
Sebastian Delgado <sebastiandelgado@Sebastians-Laptop.local>
2024-05-15 22:14:58 +0200

    make nvimtree nicer

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -16,14 +16,28 @@ nvim_tree.setup {
     update_cwd = true,
   },
   renderer = {
+    add_trailing = true,
+    highlight_git = true,
+    highlight_opened_files = "icon",
     root_folder_modifier = ":t",
+    indent_markers = {
+      enable = true,
+    },
     icons = {
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
       glyphs = {
         default = "",
         symlink = "",
         folder = {
-          arrow_open = "",
-          arrow_closed = "",
+          arrow_open = "",  -- Can use different arrow icons
+          arrow_closed = ">", -- Can use different arrow icons
           default = "",
           open = "",
           empty = "",
@@ -54,14 +68,25 @@ nvim_tree.setup {
     },
   },
   view = {
-    width = 30,
+    width = 35,  -- Slightly wider for better readability
     side = "left",
+    number = false,
+    relativenumber = false,
     mappings = {
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = "x", cb = tree_cb "split" },  -- Added horizontal split mapping
+        { key = "t", cb = tree_cb "tabnew" },  -- Added open in new tab mapping
       },
     },
+  },
+  git = {
+    enable = true,
+    ignore = false,  -- Show ignored files
+  },
+  filters = {
+    dotfiles = false,  -- Show dotfiles
   },
 }
